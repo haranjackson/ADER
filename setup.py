6f680bb6101806355d47278aa527e40faab4bffc
Haran Jackson <jackson.haran@gmail.com>
2018-04-05 09:58:15 +0100

    added setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ader',
+  packages = ['ader'],
+  version = '1.0',
+  description = 'The ADER method for solving any (potentially very stiff) hyperbolic system of PDEs',
+  author = 'Haran Jackson',
+  author_email = 'jackson.haran@gmail.com',
+  url = 'https://github.com/haranjackson/ADER',
+  download_url = 'https://github.com/haranjackson/ADER/archive/1.0.tar.gz',
+  keywords = ['ADER', 'WENO', 'Discontinuous Galerkin', 'Finite Volume', 'PDEs', 'Partial Differential Equations'],
+  classifiers = [],
+)

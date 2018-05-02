18b01d95eefc905d78af3b86465fe78ab910142a
Haran Jackson <jackson.haran@gmail.com>
2018-05-02 16:57:00 +0100

    version number update

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ def open_file(fname):
 setup(
   name = 'ader',
   packages = ['ader', 'ader.etc', 'ader.dg', 'ader.fv', 'ader.weno'],
-  version = '1.1.0',
+  version = '1.1.1',
   description = 'The ADER method for solving any (potentially very stiff) hyperbolic system of PDEs',
   long_description=open_file('README.rst').read(),
   author = 'Haran Jackson',
   author_email = 'jackson.haran@gmail.com',
   license="MIT",
   url = 'https://github.com/haranjackson/ADER',
-  download_url = 'https://github.com/haranjackson/ADER/archive/1.1.0.tar.gz',
+  download_url = 'https://github.com/haranjackson/ADER/archive/1.1.1.tar.gz',
   keywords = ['ADER', 'WENO', 'Discontinuous Galerkin', 'Finite Volume', 'PDEs', 'Partial Differential Equations'],
   classifiers=[
     'Intended Audience :: Science/Research',
